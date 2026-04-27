382c3c466cb4f538dda37a1c3ea3483ebe842220
jonatanufalcon-creator <jonatan.u.falcon@gmail.com>
2026-04-27 14:10:27 -0300

    Create App component displaying modules grid

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Main Application</h1>
+            <div className="modules-grid">
+                {/* Modules will be displayed here */}
+            </div>
+        </div>
+    );
+};
+
+export default App;
